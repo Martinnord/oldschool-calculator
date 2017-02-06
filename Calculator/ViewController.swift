88e9ef57eb9e3124462bb903f56e23892111ef15
Martin Nordström <martinnord@Martin-Nord.local>
2017-02-06 08:47:12 +0100

    Need to fix a 'reset' button

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,7 +11,23 @@ import AVFoundation
 
 class ViewController: UIViewController {
 
-    var btnSound = AVAudioPlayer
+    @IBOutlet weak var outputLbl: UILabel!
+    
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Add = "+"
+        case Subtract = "-"
+        case Empty = "Empty"
+    }
+    
+    var runningNumber = ""
+    var currentOperation = Operation.Empty
+    var leftValString = ""
+    var rightValString = ""
+    var result = ""
+    
+    var btnSound: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,12 +41,75 @@ class ViewController: UIViewController {
         } catch let err as NSError{
             print(err.debugDescription)
         }
+    }
+    
+    @IBAction func onDividePressed(sender: AnyObject) {
+        processOperation(operation: .Divide)
+    }
+    
+    @IBAction func onMultiplyPressed(sender: AnyObject) {
+        processOperation(operation: .Multiply)
+    }
+    
+    @IBAction func onSubtractPressed(sender: AnyObject) {
+        processOperation(operation: .Subtract)
+    }
+    
+    @IBAction func onAddPressed(sender: AnyObject) {
+        processOperation(operation: .Add)
+    }
+    
+    @IBAction func onEqualPressed(sender: AnyObject) {
+        processOperation(operation: currentOperation)
+    }
+    
+    func playSound() {
+        if btnSound.isPlaying {
+            btnSound.stop()
+        }
         
+        btnSound.play()
+    }
+    
+    @IBAction func numberPressed(sender: UIButton) {
+        playSound()
         
-        
+        runningNumber += "\(sender.tag)" // Appends the number to the runningNumber with the numbers tag.
+        outputLbl.text = runningNumber
     }
 
-
-
+    func processOperation(operation: Operation) {
+        playSound()
+        if currentOperation != Operation.Empty {
+            // A user selected an operator, but then selected another operator without first entering a number
+            if runningNumber != "" {
+                rightValString = runningNumber
+                runningNumber = ""
+                
+                if currentOperation == Operation.Multiply {
+                    result = "\(Double(leftValString)! * Double(rightValString)!)"
+                } else if currentOperation == Operation.Divide {
+                    result = "\(Double(leftValString)! / Double(rightValString)!)"
+                } else if currentOperation == Operation.Subtract {
+                    result = "\(Double(leftValString)! - Double(rightValString)!)"
+                } else if currentOperation == Operation.Add {
+                    result = "\(Double(leftValString)! + Double(rightValString)!)"
+                }
+                
+                leftValString = result
+                outputLbl.text = result
+            }
+            
+            currentOperation = operation
+        } else {
+            // This is the first time an operator has been pressed
+            leftValString = runningNumber
+            runningNumber = ""
+            currentOperation = operation
+            
+        }
+    }
+    
+    
 }
 
